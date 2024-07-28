3bcdd9336d99c7c797e43b9c27163d56e4e16994
vignesh-mvgs <vigneshbalanmvgs2003@gmail.com>
2024-07-28 17:42:11 +0530

    0.1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./Componants/Header";
+import Footer from "./Componants/Footer";
+//header //home //skills //contact //footer
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
