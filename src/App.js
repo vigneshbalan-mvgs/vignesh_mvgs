6d0a98c642784b50e263e4ad07fa2a45a3d60587
vignesh-mvgs <vigneshbalanmvgs2003@gmail.com>
2024-07-29 23:24:39 +0530

    0.2
    Setup the React-Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
-import Header from "./Componants/Header";
 import Footer from "./Componants/Footer";
-//header //home //skills //contact //footer
+import Header from "./Componants/Header";
+import Contact from "./Componants/Contact";
+import Skills from "./Componants/Skills";
+import Home from "./Componants/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <Footer></Footer>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/skills" element={<Skills />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Contact from "./Componants/Contact";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-);
+root.render(<App />);
