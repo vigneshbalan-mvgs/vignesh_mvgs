5a159fb33dbee33bb2002603805f9453048a3a82
vignesh-mvgs <vigneshbalanmvgs2003@gmail.com>
2024-07-31 20:33:45 +0530

    0.3
    begin with Home.js

--- src/Componants/Home.js
+++ src/Componants/Home.js
@@ -1,16 +1,40 @@
+import React from "react";
+import "./CSS/Home.css";
 import { Link } from "react-router-dom";
 
-function Home() {
+const Home = () => {
   return (
-    <div>
-      <Link to="https://github.com/vigneshbalan-mvgs" target="_blank">
-        <i class="fa-brands fa-github"></i>
-      </Link>
-      <Link to="https://www.linkedin.com/in/vignesh-balan-mvgs" target="_blank">
-        <i class="fa-brands fa-linkedin"></i>
-      </Link>
+    <div className="home">
+      <div className="left-container">
+        <div className="placeholder" />
+        <div className="placeholder" />
+      </div>
+      <div className="right-container">
+        <div className="circle" />
+        <Link
+          to="mailto:vigneshbalanmvgs2003@gmail.com"
+          className="link mail"
+          target="_blank"
+        >
+          <i className="fa-solid fa-envelope"></i> Mail
+        </Link>
+        <Link
+          to="https://github.com/vigneshbalan-mvgs"
+          className="link github"
+          target="_blank"
+        >
+          <i className="fa-brands fa-github"></i> GitHub
+        </Link>
+        <Link
+          to="https://www.linkedin.com/in/vignesh-balan-mvgs"
+          className="link linkedin"
+          target="_blank"
+        >
+          <i className="fa-brands fa-linkedin"></i> LinkedIn
+        </Link>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
