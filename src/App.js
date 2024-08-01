f713e4e0e6c4a776aac4e4229cbe1712ce242e86
vignesh-mvgs <vigneshbalanmvgs2003@gmail.com>
2024-08-01 08:18:11 +0530

    0.4
    Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-import Footer from "./Componants/Footer";
-import Header from "./Componants/Header";
 import Contact from "./Componants/Contact";
 import Skills from "./Componants/Skills";
 import Home from "./Componants/Home";
@@ -10,13 +8,11 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/skills" element={<Skills />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
         </Routes>
-        <Footer />
       </BrowserRouter>
     </div>
   );
